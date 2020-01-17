fb01e4157dea2317b83f214418476db0dd341f31
Daryl McAvoy <dmcavoy631@gmail.com>
2020-01-17 15:53:54 -0600

    Completed unit testin completed some bonus options

--- dom-tictactoe.js
+++ dom-tictactoe.js
@@ -7,8 +7,10 @@
 // 3. Look for the @TODO, to fix
 // next to each @TODO you will find tasks that need to be finished
 // 4. GET THIS GAME WORKING!!
+let winCntX = 0;    
+let winCntO = 0;
+let currentMarker = 'X';
 
-let currentMarker = 'X'
 let board = [
   ['','',''],
   ['','',''],
@@ -19,6 +21,8 @@ let board = [
 const handleClick = (element) => {
   // check to see if the square clicked has anything in it, if not continue
   // this prevents an X being changed to an O
+  document.getElementById("player" + currentMarker).classList.toggle("yourTurn");
+  if( )
   if(!document.getElementById(element.id).innerHTML){
     addMarker(element.id)
     updateBoard(element.id)
@@ -30,12 +34,9 @@ const addMarker = (id) => {
   console.log(`We'll place a mark on square: ${id}`)
   // @TODO, Mix & Match. 
   // You will need the following pieces:
-  
-  // = currentMarker
-  // .getElementById(id)
-  // document
-  // .innerHTML 
-  
+
+  document.getElementById(id).innerHTML = currentMarker;
+
   // Arrange the above pieces into one a single line of code
   // to add an X or O to the board to the DOM so it can be scene on the screen.
 }
@@ -50,44 +51,108 @@ const updateBoard = (id) => {
   console.log(board)
 
   // @TODO, Your code here: use the above information to change the board variable(array of arrays)
+  board[row][column] = currentMarker;
   // HINT: in your browser open up the dev tools -> console
 }
 
 const checkForWin = () => {
   // calls each checkForWin possibility and if any are true gives a page alert,
   if(horizontalWin() || verticalWin() || diagonalWin()) {
-    // **BONUS** you could make the dismissal of this alert window reset the board...
+// **BONUS** you could make the dismissal of this alert window reset the board...
     window.alert(`Player ${currentMarker} won!`)
-  } else {
-    // if no win, change the marker from X to O, or O to X for the next player.
-    changeMarker()
+    resetBoard()
+
+    //Update Score and innerHTML values
+    if(currentMarker == 'X'){
+
+      winCntX++;
+      document.getElementById('scoreX').innerHTML = winCntX;
+    }
+    //Update Score and innerHTML values
+    if(currentMarker == 'O'){
+
+      winCntO++;
+      document.getElementById('scoreO').innerHTML = winCntO;
+
   }
+
+} else {
+  // if no win, change the marker from X to O, or O to X for the next player.
+
+    changeMarker()
+  // window.alert(`Player ${currentMarker} Turn!`)
+
+}
 }
 
 const horizontalWin = () => {
   // @TODO, Your code here: to check for horizontal wins
+  let HM = currentMarker 
+  for (let i = 0; i < board.length; i++){
+    let j = 0;
+    if (board[i][j] == HM && board[i][j+1] == HM && board[i][j+2] == HM){
+      console.log(board[i][j])
+      return true;
+    }
+  }
 }
+
 
 const verticalWin = () => {
   // @TODO, Your code here: to check for vertical wins
+  let VM = currentMarker 
+  for (let j = 0; j < board.length; j++){
+    let i = 0;
+    if (board[i][j] == VM && board[i+1][j] == VM && board[i+2][j] == VM){
+      console.log(board[i][j])
+      return true;
+    }
+  }
 }
 
 const diagonalWin = () => {
   // @TODO, Your code here: to check for diagonal wins
+  let XM = currentMarker;
+  let i = 0;
+  let j = 0;
+
+  if (board[i][j] == XM && board[i+1][j+1] == XM && board[i+2][j+2] == XM){
+    console.log(board[i][j])
+    return true;
+  } else if (board[i][j+2] == XM && board[i+1][j+1] == XM && board[i+2][j] == XM){
+    console.log(board[i][j])
+    return true;
+  }
 }
+
 
 const changeMarker = () => {
   // ternary operator: if it's an X make it an O, if O make it an X
-  currentMarker = currentMarker === "X" ? "O" : "X"
+
+  currentMarker = currentMarker === "X" ? "O" : "X";
+ 
 }
 
 const resetBoard = () => {
   // sanity check: this tells us the function is being called
   console.log("the board was cleared!")
 
+  // Clearing ScoreBoard
+  winCntX = 0;    
+  winCntO = 0;
+  document.getElementById('scoreX').innerHTML = winCntX
+  document.getElementById('scoreO').innerHTML = winCntO
+  document.getElementById("playerO").classList.remove("yourTurn");
+  document.getElementById("playerX").classList.remove("yourTurn");
+
+
   // collects all of the "td"s into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
   const squares = document.getElementsByTagName("TD")
-  
+  board = [
+    ['','',''],
+    ['','',''],
+    ['','','']
+  ];
   // loops over the HTML Collections and clears out the Xs and Os
   for (i=0; i<squares.length; i++) {
     console.log(squares[i])
--- main.js
+++ main.js
@@ -34,23 +34,66 @@ const printBoard = () => {
 
 const horizontalWin = () => {
   // Your code here to check for horizontal wins
+  let HM = playerTurn;
+  for (let i = 0; i < board.length; i++){
+    let j = 0;
+    if (board[i][j] == HM && board[i][j+1] == HM && board[i][j+2] == HM){
+      return true;
+    }
+  }
 }
 
 const verticalWin = () => {
   // Your code here to check for vertical wins
+  let VM = playerTurn;
+  for (let j = 0; j < board.length; j++){
+    let i = 0;
+    if (board[i][j] == VM && board[i+1][j] == VM && board[i+2][j] == VM){
+      return true;
+    }
+  }
 }
 
 const diagonalWin = () => {
   // Your code here to check for diagonal wins
+  let XM = playerTurn;
+  let i = 0;
+  let j = 0;
+
+  if (board[i][j] == XM && board[i+1][j+1] == XM && board[i+2][j+2] == XM){
+    console.log(board[i][j])
+    return true;
+  } else if (board[i][j+2] == XM && board[i+1][j+1] == XM && board[i+2][j] == XM){
+    console.log(board[i][j])
+    return true;
+  }
 }
+
 
 const checkForWin = () => {
   // Your code here call each of the check for types of wins
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 const ticTacToe = (row, column) => {
   // Your code here to place a marker on the board
+
+  board[row][column] = playerTurn;
+  
   // then check for a win
+  checkForWin()
+
+  // Change player
+  changePlayer()
+}
+
+// If X make it O
+const changePlayer = () =>{
+  playerTurn = playerTurn === "X" ? "O" : "X";
 }
 
 const getPrompt = () => {
@@ -73,11 +116,11 @@ if (typeof describe === 'function') {
   describe('#ticTacToe()', () => {
     it('should place mark on the board', () => {
       ticTacToe(1, 1);
-      assert.deepEqual(board, [ [' ', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
+      assert.deepStrictEqual(board, [ [' ',' ',' '], [' ','X',' '], [' ',' ',' '] ]);
     });
     it('should alternate between players', () => {
       ticTacToe(0, 0);
-      assert.deepEqual(board, [ ['O', ' ', ' '], [' ', 'X', ' '], [' ', ' ', ' '] ]);
+      assert.deepStrictEqual(board, [ ['O',' ',' '], [' ','X',' '], [' ',' ',' '] ]);
     });
     it('should check for vertical wins', () => {
       board = [ [' ', 'X', ' '], [' ', 'X', ' '], [' ', 'X', ' '] ];
